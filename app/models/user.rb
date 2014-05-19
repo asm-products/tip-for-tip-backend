7794d4190bb263317fe0e817c6209e79f1d96cc0
Amos Lanka <amoslanka@gmail.com>
2014-05-19 02:23:12 -0700

    add partner association to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   has_many :identities
   has_many :tips
+  has_one  :partner
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
