83fbbf15e41922e54f8a4d6084bac14486e07384
Amos Lanka <amoslanka@gmail.com>
2014-05-21 00:46:34 -0700

    derp

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   class UsersController < ApplicationController
     include TokenAuthentication
 
-    before_filter :authenticate_user_from_token!
+    before_filter :authenticate_user_from_token
     before_filter :authenticate_user!
 
     def profile
