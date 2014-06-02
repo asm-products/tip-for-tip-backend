0249828a875ee30272536a90830fafd991ddd577
Amos Lanka <amoslanka@gmail.com>
2014-06-01 22:28:33 -0700

    Update spec_helper for rspec 3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
 require 'webmock/rspec'
@@ -18,7 +17,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
