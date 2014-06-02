8ba3c8898ecce3a1030c35e6904c338361c9b27d
Amos Lanka <amoslanka@gmail.com>
2014-06-01 23:36:56 -0700

    Specs for api GET nouns/places/foursquare/:id, and cleanup. It handles foursquare errors now.

--- app/controllers/api/nouns/places_controller.rb
+++ app/controllers/api/nouns/places_controller.rb
@@ -5,6 +5,8 @@ class Api::Nouns::PlacesController < ApplicationController
   before_filter :authenticate_user_from_token
   before_filter :authenticate_user!
 
+  rescue_from Foursquare2::APIError, with: :handle_foursquare_api_error
+
   def show
     id = show_params[:place_id]
     @place = ::Nouns::Place.where('id = ? OR uuid = ?', id, id).first
@@ -13,7 +15,11 @@ class Api::Nouns::PlacesController < ApplicationController
 
   def foursquare_show
     @place = ::Nouns::Place.find_by foursquare_id: foursquare_show_params[:foursquare_id]
-    @place ||= fetch_and_create_from_foursquare(foursquare_show_params[:foursquare_id])
+
+    unless @place
+      response.status = :created
+      @place = fetch_and_create_from_foursquare(foursquare_show_params[:foursquare_id])
+    end
 
     render :show
   end
@@ -30,11 +36,22 @@ class Api::Nouns::PlacesController < ApplicationController
     params.permit(:foursquare_id)
   end
 
-  def fetch_and_create_from_foursquare(id)
+  def fetch_and_create_from_foursquare id
     data = foursquare_client.venue id
     place = ::Nouns::Place.create_from_foursquare! data
     logger.info "Record created for Foursquare place: #{place.foursquare_id} (#{place.name})"
     place
+  end
+
+  def handle_foursquare_api_error e
+    code = e.code.to_i
+    # We treat url param and body param errors differently.
+    if code == 400 && e.message =~ /is invalid for venue id/
+      render status: :not_found, json: { error: :not_found }
+    else
+      logger.warn "TODO: handle error responses better."
+      render status: code, json: { error: e.message }
+    end
   end
 
 end
--- spec/controllers/api/nouns/places_controller_spec.rb
+++ spec/controllers/api/nouns/places_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+include Support::FoursquareMock
 
 describe Api::Nouns::PlacesController do
   let!(:user) { user = stub_token_authentication }
@@ -47,17 +48,38 @@ describe Api::Nouns::PlacesController do
       let(:place) { FactoryGirl.create :place }
 
       it 'responds successfully' do
-        get :show, place_id: place.id
+        get :foursquare_show, foursquare_id: place.foursquare_id
         expect(response.status).to eq 200
       end
 
       it 'assigns the place' do
-        get :show, place_id: place.id
+        get :foursquare_show, foursquare_id: place.foursquare_id
         expect(assigns :place).to eq place
       end
     end
 
     context 'if not found' do
+      let!(:venue_id) { mock_foursquare_venue[:id] }
+
+      it 'fetches data from foursquare and creates a place' do
+        expect{ get :foursquare_show, foursquare_id: venue_id }
+          .to change{ Nouns::Place.count }.by 1
+      end
+
+      it 'responds with 201 created' do
+        get :foursquare_show, foursquare_id: venue_id
+        expect(response.status).to eq 201
+      end
+
+      context 'and no data is found on foursquare' do
+        let!(:venue_id) { mock_foursquare_venue_not_found[:id] }
+
+        it 'responds with 404' do
+          get :foursquare_show, foursquare_id: venue_id
+          expect(response.status).to eq 404
+        end
+
+      end
 
     end
 
