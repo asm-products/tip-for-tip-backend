bc0f7ca73b6890b62d8977fb1fe657bf1731ecd5
Amos Lanka <amoslanka@gmail.com>
2014-06-07 14:07:58 -0700

    Add token to all redirected paths on omniauth callbacks.

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -16,24 +16,28 @@ class AuthController < Devise::OmniauthCallbacksController
   private
 
   def callback_for provider
-    omniauth_params = request.env["omniauth.params"].with_indifferent_access
-    redirect_url = omniauth_params[:redirect_to]
-
     @user = OmniauthUserFinder.new.(request.env["omniauth.auth"], current_user)
+
     if @user.persisted?
-      redirect_url ||= after_sign_in_path_for(resource)
       sign_in @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: provider.to_s.titleize) if is_navigational_format?
-      redirect_to redirect_url
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"]
-      redirect_url ||= after_sign_in_path_for(resource)
-      redirect_to redirect_url
     end
+
+    redirect_to after_sign_in_path_for(resource)
   end
 
   def after_sign_in_path_for(user)
-    profile_path
+    omniauth_params = request.env["omniauth.params"].with_indifferent_access
+    redirect_to_url = omniauth_params[:redirect_to] unless omniauth_params[:redirect_to].blank?
+    uri = URI.parse(redirect_to_url || profile_path)
+    uri.query = "#{uri.query}&token=#{token}"
+    uri.to_s
+  end
+
+  def token
+    request.env["omniauth.auth"].credentials.token
   end
 
 end
