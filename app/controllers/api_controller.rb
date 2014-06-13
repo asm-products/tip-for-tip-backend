e62d6feb2abed12154b2573dde8bdf089419e46f
Amos Lanka <amoslanka@gmail.com>
2014-06-13 14:42:11 -0700

    Rescue all unexpected exceptions in api with a 500 response.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -11,6 +11,8 @@ class ApiController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
   # rescue_from ActionController::ParameterMissing #TODO: strong params rescue
   # rescue_from ActiveRecord::RecordInvalid #TODO 422 responses
+  # Catches all remaining errors
+  rescue_from Exception, with: :server_error
 
   protected
 
@@ -23,4 +25,12 @@ class ApiController < ApplicationController
     # TODO: better response error message
     render status: :not_found, json: { error: :not_found }
   end
+
+  def server_error e=nil
+    # TODO: better response error messages
+    # TODO: submit errors from there to bugsnag or something similar.
+    logger.error "Unexpected server error, responding with 500: \n#{e.message}\n#{e.backtrace}"
+    render status: 500, json: { error: :server_error, message: "Unexpected server error" }
+  end
+
 end
